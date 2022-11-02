68d7188f2f4d73cb11fb145335b7e6c49148e2e8
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-11-02 17:58:04 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,6 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+.bootstrapModule(AppModule)
+.catch((err) => console.error(err));
+let lista: any[] = [1, true, "Cadena"];
