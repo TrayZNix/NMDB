fd50058852d0f1387982c2c25fdbf015250326c8
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-11-03 11:05:29 +0100

    Hotfix version de angular

--- src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
+++ src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
@@ -1,19 +1,18 @@
-import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+
 import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
-import { Router } from "@angular/router";
 import { createPopper } from "@popperjs/core";
 import {
   ResponseAccountDetails,
   ResponseDeleteSession,
 } from "src/app/interfaces/authorization.interface";
 import { AuthService } from "src/app/Services/auth.service";
-import Swal from "sweetalert2";
+
 @Component({
   selector: "app-user-dropdown",
   templateUrl: "./user-dropdown.component.html",
 })
 export class UserDropdownComponent implements AfterViewInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService) {}
   dropdownPopoverShow = false;
   accDetails!: ResponseAccountDetails;
   @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
